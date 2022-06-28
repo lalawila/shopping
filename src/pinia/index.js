a84a9c991d2b19fd9c2bbe4fce12166eb77acd8c
lalawila <wangosky@gmail.com>
2022-06-28 19:35:26 +0800

    持久化

--- src/pinia/index.js
+++ src/pinia/index.js
@@ -2,6 +2,7 @@ import { createPinia } from "pinia"
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate"
 
 const pinia = createPinia()
-pinia.use(piniaPluginPersistedstate)
+
+pinia.use(piniaPluginPersistedstate) // 绑定持久化插件
 
 export default pinia
